73bf1fe053ae889f6a1ba96471c2e0e5173c728b
Roger Versluis <roger.versluis@fieldcap.ca>
2020-08-30 20:30:31 -0600

    Spruced up readme, added code to detect if current file has already been exported.

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# VsCode Export-This
+# Export-This for Visual Studio Code
 
-A VSCode extension to easily and quickly export definitions to an index.ts file.
+An extension for Visual Studio code to easily export TypeScript definitions to your index.ts files.
 
-This extension searches for the nearst index.ts file and adds the current file to it, it will not search further than the opened workspace.
+![Demo](./resources/demo.gif)
+
+## Install
+
+1. Open **Extensions** sideBar panel in Visual Studio Code and choose the menu options for **View → Extensions**
+1. Search for `Export-This`
+1. Click **Install**
+1. Click **Reload**, if required
+
+## Quick Usage
+
+1. Create an index.ts file next to or in a folder under the TypeScript file you are working on.
+1. The index.ts file can be as deep as you like, as long as it is in your workspace folder.
+1. Go to the declaration you wish to export (class, interface, enum etc.)
+1. Hit the quick fix **Ctrl+Period** and select **Export file**.
+1. The index.ts file will now be opened and the current file will be expored.
+1. Hit save and done!
+
+## Issues
+
+Feel free to report an issues or feature requests!
--- src/extension.ts
+++ src/extension.ts
@@ -7,131 +7,56 @@ const EXPORT_THIS_MENTION = 'export_this_mention';
 
 const EXPORT = 'export';
 
-export function activate(context: vscode.ExtensionContext) {
-    const exportThisDiagnostics = vscode.languages.createDiagnosticCollection(
-        'export_this'
-    );
+export async function activate(context: vscode.ExtensionContext) {
+    const exportThisDiagnostics = vscode.languages.createDiagnosticCollection('export_this');
     context.subscriptions.push(exportThisDiagnostics);
 
-    subscribeToDocumentChanges(context, exportThisDiagnostics);
+    await subscribeToDocumentChanges(context, exportThisDiagnostics);
 
     context.subscriptions.push(
-        vscode.languages.registerCodeActionsProvider(
-            'typescript',
-            new ExportThisInfo(),
-            {
-                providedCodeActionKinds: ExportThisInfo.providedCodeActionKinds,
-            }
-        )
+        vscode.languages.registerCodeActionsProvider('typescript', new ExportThisInfo(), {
+            providedCodeActionKinds: ExportThisInfo.providedCodeActionKinds,
+        })
     );
 
     context.subscriptions.push(
         vscode.commands.registerCommand(COMMAND, async () => {
-            // Check if a workspace is loaded, if not, exit early.
-            const wss = vscode.workspace.workspaceFolders;
-
-            if (!wss || !wss[0]) {
-                vscode.window.showErrorMessage(`No folder/workspace open.`);
-                return;
-            }
-
-            const workspaceUri = wss[0].uri;
-
-            const filePath = vscode.window.activeTextEditor?.document.uri;
-
-            if (!filePath) {
-                vscode.window.showErrorMessage(`Error loading active file.`);
-                return;
-            }
-
-            // Check the current folder first.
-            let indexPath = posix.join(filePath.path, '..', 'index.ts');
-
-            let indexUri = vscode.Uri.file(indexPath);
-
-            if (!indexUri) {
-                vscode.window.showErrorMessage(
-                    `Error constructing index.ts file path.`
-                );
-                return;
-            }
-
-            // Go down 1 folder each time and check for an index.ts file.
-            let found: boolean = false;
-
-            // Failsafe.
-            let iteration: number = 0;
-
-            while (!found && iteration < 100) {
-                indexUri = indexUri.with({ path: indexPath });
-
-                try {
-                    await vscode.workspace.fs.stat(indexUri);
-                    found = true;
-                } catch {
-                    // Go down 1 folder.
-                    indexPath = posix.join(
-                        indexUri.path,
-                        '..',
-                        '..',
-                        'index.ts'
-                    );
-
-                    if (indexPath.indexOf(workspaceUri.path) === -1) {
-                        break;
-                    }
-
-                    iteration++;
-                }
-            }
+            const indexUri = await findIndex();
 
             // If no index.ts was found, show an error and exit.
-            if (!found) {
-                vscode.window.showErrorMessage(
-                    'Unable to find an index.ts file to export to'
-                );
+            if (!indexUri) {
+                vscode.window.showErrorMessage('Unable to find an index.ts file to export to');
                 return;
             }
 
             // Add the export text to the end of the file.
-            const fileDir = posix.dirname(filePath.path);
-            const fileName = posix.basename(filePath.path, '.ts');
-            const indexDir = posix.dirname(indexUri.path);
-            const relativePath = fileDir.replace(indexDir, '');
+            const relPath = getRelativePath(indexUri);
 
-            const exportText = `export * from '.${relativePath}/${fileName}';`;
+            if (relPath == null) {
+                vscode.window.showErrorMessage('Unable to construct the relative path');
+                return;
+            }
 
-            vscode.workspace.openTextDocument(indexUri).then(
-                (textDocument: vscode.TextDocument) => {
-                    vscode.window
-                        .showTextDocument(textDocument, 1, false)
-                        .then((e) => {
-                            e.edit((edit) => {
-                                edit.insert(
-                                    new vscode.Position(
-                                        textDocument.lineCount,
-                                        0
-                                    ),
-                                    exportText
-                                );
-                            });
-                        });
-                },
-                (error: any) => {
-                    console.error(error);
-                    vscode.window.showErrorMessage(
-                        `Error inserting export snippet.`
-                    );
-                }
-            );
+            const exportText = `export * from '.${relPath}';`;
+
+            const textDocument = await vscode.workspace.openTextDocument(indexUri);
+
+            try {
+                const e = await vscode.window.showTextDocument(textDocument, 1, false);
+
+                e.edit((edit) => {
+                    edit.insert(new vscode.Position(textDocument.lineCount, 0), exportText);
+                });
+            } catch (error) {
+                console.error(error);
+                vscode.window.showErrorMessage(`Error inserting export snippet.`);
+            }
         })
     );
 }
 
 export class ExportThisInfo implements vscode.CodeActionProvider {
-    public static readonly providedCodeActionKinds = [
-        vscode.CodeActionKind.QuickFix,
-    ];
+    public static readonly providedCodeActionKinds = [vscode.CodeActionKind.QuickFix];
 
     provideCodeActions(
         document: vscode.TextDocument,
@@ -144,18 +69,12 @@ export class ExportThisInfo implements vscode.CodeActionProvider {
             .map((diagnostic) => this.createCommandCodeAction(diagnostic));
     }
 
-    private createCommandCodeAction(
-        diagnostic: vscode.Diagnostic
-    ): vscode.CodeAction {
-        const action = new vscode.CodeAction(
-            'Export file',
-            vscode.CodeActionKind.QuickFix
-        );
+    private createCommandCodeAction(diagnostic: vscode.Diagnostic): vscode.CodeAction {
+        const action = new vscode.CodeAction('Export file', vscode.CodeActionKind.QuickFix);
         action.command = {
             command: COMMAND,
             title: 'Export file',
-            tooltip:
-                'This will export the current file to the nearst index.ts file.',
+            tooltip: 'This will export the current file to the nearst index.ts file.',
         };
         action.diagnostics = [diagnostic];
         action.isPreferred = false;
@@ -163,74 +82,154 @@ export class ExportThisInfo implements vscode.CodeActionProvider {
     }
 }
 
-export function refreshDiagnostics(
+async function refreshDiagnostics(
     doc: vscode.TextDocument,
     diagnosticsCollection: vscode.DiagnosticCollection
-): void {
-    const diagnostics: vscode.Diagnostic[] = [];
+): Promise<void> {
+    const fileUri = vscode.Uri.file(doc.fileName);
+
+    const fileName = posix.basename(fileUri.path);
+    if (fileName === 'index.ts') {
+        return;
+    }
+
+    let diagnostics: vscode.Diagnostic[] = [];
 
     for (let lineIndex = 0; lineIndex < doc.lineCount; lineIndex++) {
         const lineOfText = doc.lineAt(lineIndex);
         if (lineOfText.text.includes(EXPORT)) {
-            diagnostics.push(createDiagnostic(doc, lineOfText, lineIndex));
+            // Find where in the line of the word 'export' is mentioned.
+            const index = lineOfText.text.indexOf(EXPORT);
+
+            // Check if this declaration has already been exported
+            const indexUri = await findIndex();
+
+            // If no index.ts can be found at all, don't even highlight.
+            if (indexUri == null) {
+                return;
+            }
+
+            const relPath = getRelativePath(indexUri);
+
+            if (relPath == null) {
+                return;
+            }
+
+            const textDocument = await vscode.workspace.openTextDocument(indexUri);
+
+            const text = textDocument.getText();
+
+            if (text.indexOf(relPath) === -1) {
+                // Create range that represents, where in the document the word is
+                const range = new vscode.Range(lineIndex, index, lineIndex, index + EXPORT.length);
+
+                const diagnostic = new vscode.Diagnostic(
+                    range,
+                    'Export file',
+                    vscode.DiagnosticSeverity.Hint
+                );
+                diagnostic.code = EXPORT_THIS_MENTION;
+
+                diagnostics.push(diagnostic);
+            }
         }
     }
 
     diagnosticsCollection.set(doc.uri, diagnostics);
 }
 
-function createDiagnostic(
-    doc: vscode.TextDocument,
-    lineOfText: vscode.TextLine,
-    lineIndex: number
-): vscode.Diagnostic {
-    // find where in the line of thet the 'export' is mentioned
-    const index = lineOfText.text.indexOf(EXPORT);
-
-    // create range that represents, where in the document the word is
-    const range = new vscode.Range(
-        lineIndex,
-        index,
-        lineIndex,
-        index + EXPORT.length
-    );
-
-    const diagnostic = new vscode.Diagnostic(
-        range,
-        'Export file',
-        vscode.DiagnosticSeverity.Hint
-    );
-    diagnostic.code = EXPORT_THIS_MENTION;
-    return diagnostic;
-}
-
-export function subscribeToDocumentChanges(
+async function subscribeToDocumentChanges(
     context: vscode.ExtensionContext,
-    emojiDiagnostics: vscode.DiagnosticCollection
-): void {
+    diagnosticCollection: vscode.DiagnosticCollection
+): Promise<void> {
     if (vscode.window.activeTextEditor) {
-        refreshDiagnostics(
-            vscode.window.activeTextEditor.document,
-            emojiDiagnostics
-        );
+        await refreshDiagnostics(vscode.window.activeTextEditor.document, diagnosticCollection);
     }
     context.subscriptions.push(
-        vscode.window.onDidChangeActiveTextEditor((editor) => {
+        vscode.window.onDidChangeActiveTextEditor(async (editor) => {
             if (editor) {
-                refreshDiagnostics(editor.document, emojiDiagnostics);
+                await refreshDiagnostics(editor.document, diagnosticCollection);
             }
         })
     );
 
     context.subscriptions.push(
-        vscode.workspace.onDidChangeTextDocument((e) =>
-            refreshDiagnostics(e.document, emojiDiagnostics)
+        vscode.workspace.onDidChangeTextDocument(
+            async (editor) => await refreshDiagnostics(editor.document, diagnosticCollection)
         )
     );
 
     context.subscriptions.push(
-        vscode.workspace.onDidCloseTextDocument((doc) =>
-            emojiDiagnostics.delete(doc.uri)
-        )
+        vscode.workspace.onDidCloseTextDocument((doc) => diagnosticCollection.delete(doc.uri))
     );
+}
+
+async function findIndex(): Promise<vscode.Uri | null | undefined> {
+    // Check if a workspace is loaded, if not, exit early.
+    const wss = vscode.workspace.workspaceFolders;
+
+    if (!wss || !wss[0]) {
+        vscode.window.showErrorMessage(`No folder/workspace open.`);
+        return null;
+    }
+
+    const workspaceUri = wss[0].uri;
+
+    const filePath = vscode.window.activeTextEditor?.document.uri;
+
+    if (!filePath) {
+        vscode.window.showErrorMessage(`Error loading active file.`);
+        return null;
+    }
+
+    // Check the current folder first.
+    let indexPath = posix.join(filePath.path, '..', 'index.ts');
+
+    let indexUri = vscode.Uri.file(indexPath);
+
+    if (!indexUri) {
+        vscode.window.showErrorMessage(`Error constructing index.ts file path.`);
+        return null;
+    }
+
+    // Go down 1 folder each time and check for an index.ts file.
+    let found: boolean = false;
+
+    // Failsafe.
+    let iteration: number = 0;
+
+    while (!found && iteration < 100) {
+        indexUri = indexUri.with({ path: indexPath });
+
+        try {
+            await vscode.workspace.fs.stat(indexUri);
+            found = true;
+        } catch {
+            // Go down 1 folder.
+            indexPath = posix.join(indexUri.path, '..', '..', 'index.ts');
+
+            if (indexPath.indexOf(workspaceUri.path) === -1) {
+                break;
+            }
+
+            iteration++;
+        }
+    }
+
+    return indexUri;
+}
+
+function getRelativePath(indexUri: vscode.Uri): string | null {
+    const filePath = vscode.window.activeTextEditor?.document.uri;
+
+    if (!filePath) {
+        return null;
+    }
+
+    const fileDir = posix.dirname(filePath.path);
+    const fileName = posix.basename(filePath.path, '.ts');
+    const indexDir = posix.dirname(indexUri.path);
+    const relativePath = fileDir.replace(indexDir, '');
+
+    return `${relativePath}/${fileName}`;
 }
